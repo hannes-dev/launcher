30b81b8efcff1251eb9d08f96dda8ab3b836846f
Hannes <hannes-dev@mailbox.org>
2026-02-03 14:45:43 +0100

    store search results so searching is not repeated every draw

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use iced::{
-    application::IntoBoot,
-    widget::{Column, column, text, text_input},
-};
+use iced::widget::{Column, column, text, text_input};
 
 use crate::{
     plugins::text_search::TextSearch,
@@ -18,19 +15,34 @@ pub fn main() -> iced::Result {
 struct State {
     plugins: Vec<Box<dyn QueryPlugin>>,
     query: String,
+    results: Vec<QueryResult>,
 }
 
 impl State {
     fn new() -> Self {
-        Self {
+        let mut state = Self {
             plugins: vec![Box::new(TextSearch)],
             query: String::new(),
-        }
+            results: Vec::new(),
+        };
+        state.update_results();
+        state
+    }
+
+    fn update_results(&mut self) {
+        self.results = self
+            .plugins
+            .iter()
+            .flat_map(|p| p.search(&self.query))
+            .collect();
     }
 
     fn update(&mut self, message: Message) {
         match message {
-            Message::ContendChanged(content) => self.query = content,
+            Message::ContendChanged(content) => {
+                self.query = content;
+                self.update_results();
+            }
         }
     }
 
@@ -38,10 +50,9 @@ impl State {
         let mut column =
             column![text_input("search...", &self.query).on_input(Message::ContendChanged)];
 
-        for plugin in &self.plugins {
-            for result in plugin.search(&self.query) {
-                column = column.push(text(result.text));
-            }
+        for result in &self.results {
+            let text = text(&result.text);
+            column = column.push(text)
         }
 
         column
