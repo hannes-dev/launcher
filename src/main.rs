cc13f61abcf0877aecb2ea29cf33189111c766f9
Hannes <hannes-dev@mailbox.org>
2026-02-03 16:46:18 +0100

    listen to keys to select an option

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use iced::widget::{Column, column, text, text_input};
+use iced::{
+    Color,
+    keyboard::{self, key::Named},
+    widget::{Column, column, text, text_input},
+};
 
 use crate::{
     plugins::text_search::TextSearch,
@@ -9,21 +13,24 @@ mod plugins;
 mod queriable;
 
 pub fn main() -> iced::Result {
-    iced::application(|| State::new(), State::update, State::view).run()
+    iced::application(|| State::new(), State::update, State::view)
+        .subscription(State::subscription)
+        .run()
 }
 
+#[derive(Default)]
 struct State {
     plugins: Vec<Box<dyn QueryPlugin>>,
     query: String,
     results: Vec<QueryResult>,
+    selected: usize,
 }
 
 impl State {
     fn new() -> Self {
         let mut state = Self {
             plugins: vec![Box::new(TextSearch)],
-            query: String::new(),
-            results: Vec::new(),
+            ..Default::default()
         };
         state.update_results();
         state
@@ -40,8 +47,16 @@ impl State {
     fn update(&mut self, message: Message) {
         match message {
             Message::ContendChanged(content) => {
+                dbg!(&content);
                 self.query = content;
                 self.update_results();
+            }
+            Message::SelectUp => self.selected = self.selected.saturating_sub(1),
+            Message::SelectDown => {
+                self.selected = self
+                    .selected
+                    .saturating_add(1)
+                    .clamp(0, self.results.len() - 1)
             }
         }
     }
@@ -50,16 +65,40 @@ impl State {
         let mut column =
             column![text_input("search...", &self.query).on_input(Message::ContendChanged)];
 
-        for result in &self.results {
-            let text = text(&result.text);
+        for (i, result) in self.results.iter().enumerate() {
+            let mut text = text(&result.text);
+            if self.selected == i {
+                text = text.color(Color::from_rgb(1., 0., 0.));
+            }
             column = column.push(text)
         }
 
         column
     }
+
+    fn subscription(&self) -> iced::Subscription<Message> {
+        keyboard::listen().filter_map(|ev| match ev {
+            keyboard::Event::KeyPressed {
+                key,
+                modified_key,
+                physical_key,
+                location,
+                modifiers,
+                text,
+                repeat,
+            } => match key.as_ref() {
+                keyboard::Key::Named(Named::ArrowUp) => Some(Message::SelectUp),
+                keyboard::Key::Named(Named::ArrowDown) => Some(Message::SelectDown),
+                _ => None,
+            },
+            _ => None,
+        })
+    }
 }
 
 #[derive(Debug, Clone)]
 enum Message {
+    SelectUp,
+    SelectDown,
     ContendChanged(String),
 }
