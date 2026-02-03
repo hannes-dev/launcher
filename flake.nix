fbcd519fd9efb1e4f8736805a6bf92db5924390e
Hannes <hannes-dev@mailbox.org>
2026-02-03 13:19:05 +0100

    basic search box

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        buildInputs = with pkgs; [
+          wayland
+          libxkbcommon
+          libGL
+        ];
+      in {
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs;
+
+          LD_LIBRARY_PATH =
+            builtins.foldl' (a: b: "${a}:${b}/lib") "${pkgs.vulkan-loader}/lib" buildInputs;
+        };
+      }
+    );
+}
---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use iced::widget::{Column, column, text, text_input};
+
+const TERMS: [&'static str; 11] = [
+    "cheese", "potato", "carrot", "house", "bear", "clobber", "turkey", "mouse", "crab", "curtain",
+    "zeus",
+];
+
+pub fn main() -> iced::Result {
+    iced::run(Counter::update, Counter::view)
+}
+
+#[derive(Default)]
+struct Counter {
+    value: i64,
+    query: String,
+}
+
+impl Counter {
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => self.value += 1,
+            Message::Decrement => self.value -= 1,
+            Message::ContendChanged(content) => self.query = content,
+        }
+    }
+
+    fn view(&self) -> Column<Message> {
+        let mut column =
+            column![text_input("search...", &self.query).on_input(Message::ContendChanged)];
+
+        for value in TERMS {
+            if value.starts_with(&self.query) {
+                column = column.push(text(value));
+            }
+        }
+
+        column
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Increment,
+    Decrement,
+    ContendChanged(String),
+}
